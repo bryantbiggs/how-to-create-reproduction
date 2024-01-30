dc0ff5607742e12ad1f49ddd4d4db55e92556cf8
Todd Thomas <tthomas@vivsoft.io>
2024-01-30 10:22:02 -0800

    bumped module version in helm-test.tf

--- helm-test.tf
+++ helm-test.tf
@@ -1,9 +1,18 @@
-################################################################################
-# TEST Cluster COMMs by deploying the Metrics Server Addon
-################################################################################
+/*
+  ----------------------------------------------------------------------------------------------------------------------
+  TEST Cluster COMMs by deploying the Metrics Server Addon
+  https://github.com/aws-ia/terraform-aws-eks-blueprints-addons/releases
+  ----------------------------------------------------------------------------------------------------------------------
+*/
+
 #module "eks_blueprints_addons" {
 #  source  = "aws-ia/eks-blueprints-addons/aws"
-#  version = "~> 1.12.0" #ensure to update this to the latest/desired version
+#  version = "~> 1.13.0" # update to the latest/desired version
 #
-#  enable metrics_server = true
+#  enable_metrics_server = true
+#
+#  cluster_name      = module.eks.cluster_name
+#  cluster_endpoint  = module.eks.cluster_endpoint
+#  cluster_version   = module.eks.cluster_version
+#  oidc_provider_arn = module.eks.oidc_provider_arn
 #}
